94526cd0fba755c1539cf4809c49b70a562025d0
00hongjun <00hongjun@gmail.com>
2022-06-26 18:04:16 +0900

    refactor: 추상화 리팩토링 class 교체

--- src/main/java/nextstep/auth/authentication/BasicAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/BasicAuthenticationFilter.java
@@ -4,47 +4,32 @@ import nextstep.auth.context.Authentication;
 import nextstep.auth.context.SecurityContextHolder;
 import nextstep.member.application.LoginMemberService;
 import nextstep.member.domain.LoginMember;
-import org.apache.tomcat.util.codec.binary.Base64;
-import org.springframework.web.servlet.HandlerInterceptor;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+public class BasicAuthenticationFilter extends ChainAuthenticationInterceptor {
 
-public class BasicAuthenticationFilter implements HandlerInterceptor {
-    private LoginMemberService loginMemberService;
+    private final LoginMemberService loginMemberService;
 
-    public BasicAuthenticationFilter(LoginMemberService loginMemberService) {
+    public BasicAuthenticationFilter(LoginMemberService loginMemberService,
+                                     AuthenticationConverter authenticationConverter) {
+        super(authenticationConverter);
         this.loginMemberService = loginMemberService;
     }
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        try {
-            String authCredentials = AuthorizationExtractor.extract(request, AuthorizationType.BASIC);
-            String authHeader = new String(Base64.decodeBase64(authCredentials));
+    public void afterAuthentication(AuthenticationToken token) {
 
-            String[] splits = authHeader.split(":");
-            String principal = splits[0];
-            String credentials = splits[1];
-
-            AuthenticationToken token = new AuthenticationToken(principal, credentials);
-
-            LoginMember loginMember = loginMemberService.loadUserByUsername(token.getPrincipal());
-            if (loginMember == null) {
-                throw new AuthenticationException();
-            }
-
-            if (!loginMember.checkPassword(token.getCredentials())) {
-                throw new AuthenticationException();
-            }
-
-            Authentication authentication = new Authentication(loginMember.getEmail(), loginMember.getAuthorities());
-
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
-            return true;
-        } catch (Exception e) {
-            return true;
+        LoginMember loginMember = loginMemberService.loadUserByUsername(token.getPrincipal());
+        if (loginMember == null) {
+            throw new AuthenticationException();
         }
+
+        if (!loginMember.checkPassword(token.getCredentials())) {
+            throw new AuthenticationException();
+        }
+
+        Authentication authentication = new Authentication(loginMember.getEmail(), loginMember.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
     }
+
 }
--- src/main/java/nextstep/auth/authentication/BearerTokenAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/BearerTokenAuthenticationFilter.java
@@ -3,39 +3,31 @@ package nextstep.auth.authentication;
 import nextstep.auth.context.Authentication;
 import nextstep.auth.context.SecurityContextHolder;
 import nextstep.auth.token.JwtTokenProvider;
-import org.springframework.web.servlet.HandlerInterceptor;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
-public class BearerTokenAuthenticationFilter implements HandlerInterceptor {
+public class BearerTokenAuthenticationFilter extends ChainAuthenticationInterceptor {
+
     private JwtTokenProvider jwtTokenProvider;
 
-    public BearerTokenAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+    public BearerTokenAuthenticationFilter(JwtTokenProvider jwtTokenProvider,
+                                           AuthenticationConverter authenticationConverter) {
+        super(authenticationConverter);
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        try {
-            String authCredentials = AuthorizationExtractor.extract(request, AuthorizationType.BEARER);
-            AuthenticationToken token = new AuthenticationToken(authCredentials, authCredentials);
-
-            if (!jwtTokenProvider.validateToken(token.getPrincipal())) {
-                throw new AuthenticationException();
-            }
-
-            String principal = jwtTokenProvider.getPrincipal(token.getPrincipal());
-            List<String> roles = jwtTokenProvider.getRoles(token.getPrincipal());
-
-            Authentication authentication = new Authentication(principal, roles);
-
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
-            return true;
-        } catch (Exception e) {
-            return true;
+    public void afterAuthentication(AuthenticationToken token) {
+        if (!jwtTokenProvider.validateToken(token.getPrincipal())) {
+            throw new AuthenticationException();
         }
+
+        String principal = jwtTokenProvider.getPrincipal(token.getPrincipal());
+        List<String> roles = jwtTokenProvider.getRoles(token.getPrincipal());
+
+        Authentication authentication = new Authentication(principal, roles);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
+
 }
